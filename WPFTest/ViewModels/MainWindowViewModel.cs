60e16f94d72184005b211c03051a11f11db737c0
Sergey Lubkov <ser.lubkov@gmail.com>
2021-03-06 22:59:13 +0200

    Тестирование работы модели-представления

---
+++ WPFTest/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WPFTest.ViewModels.Base;
+
+namespace WPFTest.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _title = "Тестовое приложени WPF";
+        
+        #region Заголовок окна
+        public string Title
+        {
+            get => _title;
+            //set
+            //{
+            //if (Equals(_title, value)) return;
+            //_title = value;
+            //OnPropertyChanged();
+
+            //Set(ref _title, value);
+            //}
+            set => Set(ref _title, value);
+        }
+        #endregion
+    }
+}
