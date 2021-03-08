d8dce8a8d3dbc41b13feeecd37120d43d395ff88
Sergey Lubkov <ser.lubkov@gmail.com>
2021-03-08 15:40:11 +0200

    Отдельный класс для команды закрытия приложения

---
+++ WPFTest/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WPFTest.Infrastructure.Commands.Base;
+
+namespace WPFTest.Infrastructure.Commands
+{
+    class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter) => Application.Current.Shutdown();
+    }
+}
