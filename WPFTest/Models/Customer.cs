2cb1af2f732f3c138150448515713f32e4cddee1
Sergey Lubkov <ser.lubkov@gmail.com>
2021-03-09 19:51:53 +0200

    Тестовая предметная область

---
+++ WPFTest/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFTest.Models
+{
+    internal class Customer
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string PhoneNum { get; set; }
+
+    }
+
+    internal class Group
+    {
+        public string Name { get; set; }
+        public ICollection<Customer> Customers { get; set;}
+    }
+}
