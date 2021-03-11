216f2be5250ddeed933a50fc2daeb272185af864
Sergey Lubkov <ser.lubkov@gmail.com>
2021-03-11 22:26:21 +0200

    Добавлена виртуализация и фильтрация для списка групп

--- WPFTest/Views/Windows/MainWindow.xaml.cs
+++ WPFTest/Views/Windows/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using WPFTest.Models;
 
 namespace WPFTest
 {
@@ -23,6 +14,25 @@ namespace WPFTest
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void CollectionViewSource_Filter(object sender, FilterEventArgs e)
+        {
+            if (!(e.Item is Group group)) return;
+            if (group.Name is null) return;
+
+            var filterStr = GroupNameFilterText.Text;
+            if (filterStr.Length == 0) return;
+            if (group.Name.IndexOf(filterStr, StringComparison.OrdinalIgnoreCase) >= 0) return;
+
+            e.Accepted = false;
+        }
+
+        private void GroupNameFilterText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var text_box = (TextBox)sender;
+            var collection = (CollectionViewSource)text_box.FindResource("GroupsCollection");
+            collection.View.Refresh();
         }
     }
 }
